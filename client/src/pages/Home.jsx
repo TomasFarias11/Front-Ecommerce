946f85a02caac9fbb917f125eb02a218a66d0c98
Tomas Farias <farias.tomas@outlook.com>
2022-01-20 21:13:15 -0300

    productos filtrados casi

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {getProducts} from '../actions/actionProducts.js'
+import {getProducts, getProductByCategory} from '../actions/actionProducts.js'
+import { useNavigate } from 'react-router-dom';
 
 export default function Home () {
 
     const dispatch = useDispatch();
-    const products = useSelector((state) => state.firstRed.products)
+    const Navigate = useNavigate();
+    const products = useSelector((state) => state.firstRed.products);
 
     useEffect(() => 
         dispatch(getProducts())
@@ -14,7 +16,23 @@ export default function Home () {
 
     console.log('estos son los productos', products)
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        dispatch(getProductByCategory(e.target.value))
+        Navigate("/products/category")
+    }
+
     return (
+        <div>
+
         <h1>Aca va el home con las CATEGORIAS, no los PRODUCTOS</h1>
+
+        <input type="button" value="iphone" onClick={(e) => handleClick(e)}/>
+        <input type="button" value="ipad" onClick={(e) => handleClick(e)}/>
+        <input type="button" value="watch" onClick={(e) => handleClick(e)}/>
+        <input type="button" value="airpods" onClick={(e) => handleClick(e)}/>
+        <input type="button" value="macbook" onClick={(e) => handleClick(e)}/>
+        <input type="button" value="tv" onClick={(e) => handleClick(e)}/>
+        </div>
     )
 }

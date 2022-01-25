c7250f66e66f9f0c8af65bd09464bef49bc98640
luisRocca <luis.rocca96@gmail.com>
2022-01-25 12:54:40 -0500

    esta la autenticacion con cogle tiene interfas grafica pero faltan las rutas del back

--- client/src/reducers/reducerRaro.js
+++ client/src/reducers/reducerRaro.js
@@ -1,12 +1,12 @@
 import store from '../store'
-// const initialState={
-// 	countries:[],
+const initialState={
+	countries:[],
 	
-// }
+}
 
 // console.log(store.getState())
 
-export default function reducer(state=[], action){
+export default function reducer(state=initialState, action){
 	console.log(state)
 	switch(action.type){
 		default:

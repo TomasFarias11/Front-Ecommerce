587fb4aaf70da8f7b3f35b7587f81afaf9200e10
Silvio Marquez <s.marquez@hotmail.com>
2022-01-26 11:44:08 -0300

    branch-admin

--- client/src/components/CarrouselMain.jsx
+++ client/src/components/CarrouselMain.jsx
@@ -17,6 +17,14 @@ function CarrouselMain() {
             <img src="https://i.postimg.cc/htzL8JBg/enero-macro-sliderweb-1920x640.jpg" class="d-block w-100" alt="..."/>
         </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
     );
 }

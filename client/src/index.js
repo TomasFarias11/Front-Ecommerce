e752dc8308e254ebda664c6bf6fd229672544d02
Tomas Farias <85124666+TomasFarias11@users.noreply.github.com>
2022-01-27 18:12:31 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,8 +12,10 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL =  "http://localhost:3001"
-  || process.env.REACT_APP_API;
+axios.defaults.baseURL =  
+//   "http://localhost:3001"
+//   || 
+  process.env.REACT_APP_API;
 
 
 

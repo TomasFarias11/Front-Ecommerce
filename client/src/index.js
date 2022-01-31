a568a8a14158f6c810dd2134b5cebc5284c068cb
Tomas Farias <85124666+TomasFarias11@users.noreply.github.com>
2022-01-30 21:07:11 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -11,8 +11,10 @@ import * as bootstrap from 'bootstrap';
 import axios from "axios";
 
 
-axios.defaults.baseURL =  
-"http://localhost:3001" || process.env.REACT_APP_API
+axios.defaults.baseURL =  process.env.REACT_APP_API
+
+
+// || "http://localhost:3001"
 
 
 ReactDOM.render(

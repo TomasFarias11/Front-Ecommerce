2aa91d68b950b4a113d697e87e836aac61ded6b1
Tomas Farias <farias.tomas@outlook.com>
2022-02-01 19:57:05 -0300

    listo

--- client/src/components/admin/AdminProduct.jsx
+++ client/src/components/admin/AdminProduct.jsx
@@ -27,7 +27,7 @@ function AdminProduct() {
             <div >
                 <div className="dropdown">
                 <Link to="/admin/addProduct">
-                <button type="button" class="btn btn-outline-secondary" >Agregar Productos</button>
+                <button type="button" className="btn btn-outline-secondary" >Agregar Productos</button>
                 </Link>
                 </div>
             </div>
@@ -36,14 +36,14 @@ function AdminProduct() {
     <div className=" card col-lg-8">
                     <div className="container-sm bg-image hover-overlay ripple" data-mdb-ripple-color="light" style={{ padding: 20 } } >
                         <div className="row" Style="background-color: #FAFAFA"    >
-                            <ul class="list-group list-group-flush">
+                            <ul className="list-group list-group-flush">
                                 {
                                     products.map(e=>
-                                        <li class="list-group-item" key={e.id}><p><b>ID: </b>{e.id}</p> <p><b>Nombre: </b> {e.name}</p> <p><b>Categoria: </b> {e.idCategory}</p>
+                                        <li className="list-group-item" key={e.id}><p><b>ID: </b>{e.id}</p> <p><b>Nombre: </b> {e.name}</p> <p><b>Categoria: </b> {e.idCategory}</p>
                                             <Link to={`/admin/edit/${e.id}`}>
-                                                <button class="btn btn-primary" style={{marginRight:10}}>editar</button>
+                                                <button className="btn btn-primary" style={{marginRight:10}}>editar</button>
                                             </Link>
-                                            <button class="btn btn-primary" name="id" value={e.id} onClick={(e)=>handelDetele(e)}>X</button>
+                                            <button className="btn btn-primary" name="id" value={e.id} onClick={(e)=>handelDetele(e)}>X</button>
                                         </li>
                                     )
                                 }
